57df14540b33920f1fc36caef665e044c992e4cc
Brendan Dasilva <brendan.dasilva@gmail.com>
2025-04-04 16:11:34 -0400

    updated app.config - update Apollo client link

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -6,16 +6,12 @@ import { provideHttpClient, withInterceptors } from '@angular/common/http';
 import { APOLLO_OPTIONS } from 'apollo-angular';
 import { HttpLink } from 'apollo-angular/http';
 import { InMemoryCache } from '@apollo/client/core';
-import { ApolloModule } from 'apollo-angular';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideRouter(routes),
     provideHttpClient(withInterceptors([])),
-    importProvidersFrom(
-      HttpClientModule,
-      ApolloModule // ✅ Provides the Apollo service
-    ),
+    importProvidersFrom(HttpClientModule),
     {
       provide: APOLLO_OPTIONS,
       useFactory: (httpLink: HttpLink) => {
@@ -23,6 +19,8 @@ export const appConfig: ApplicationConfig = {
           cache: new InMemoryCache(),
           link: httpLink.create({
             uri: 'https://employeemanagementapp-backend.onrender.com/graphql',
+
+            withCredentials: true,
           }),
         };
       },
