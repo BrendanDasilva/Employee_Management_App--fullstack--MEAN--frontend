3b06ab18bf0297cf1e4e7a3c8b966d7186820172
Brendan Dasilva <brendan.dasilva@gmail.com>
2025-04-04 16:22:46 -0400

    updated app.config

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,11 +1,13 @@
 import { ApplicationConfig, importProvidersFrom } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { routes } from './app.routes';
-import { HttpClientModule } from '@angular/common/http';
 import { provideHttpClient, withInterceptors } from '@angular/common/http';
+import { HttpClientModule } from '@angular/common/http';
+
 import { APOLLO_OPTIONS } from 'apollo-angular';
 import { HttpLink } from 'apollo-angular/http';
-import { InMemoryCache } from '@apollo/client/core';
+import { InMemoryCache, ApolloLink } from '@apollo/client/core';
+import { setContext } from '@apollo/client/link/context';
 
 export const appConfig: ApplicationConfig = {
   providers: [
@@ -15,13 +17,21 @@ export const appConfig: ApplicationConfig = {
     {
       provide: APOLLO_OPTIONS,
       useFactory: (httpLink: HttpLink) => {
+        const authLink = setContext((_, { headers }) => ({
+          headers,
+          credentials: 'include',
+        }));
+
+        const link = ApolloLink.from([
+          authLink,
+          httpLink.create({
+            uri: 'https://employeemanagementapp-backend.onrender.com/graphql',
+          }),
+        ]);
+
         return {
           cache: new InMemoryCache(),
-          link: httpLink.create({
-            uri: 'https://employeemanagementapp-backend.onrender.com/graphql',
-
-            withCredentials: true,
-          }),
+          link,
         };
       },
       deps: [HttpLink],
